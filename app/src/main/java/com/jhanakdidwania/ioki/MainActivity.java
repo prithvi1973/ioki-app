369a5ef34f396cea484c6eb5d75ee09a8b4ec995
Prithvi Anand <prithvi.anand1972.pa@gmail.com>
2018-06-14 10:12:17 +0530

    Updated code to work with networkUtils
    Modified IoKiLoginTask() as per new Network Utils code.

--- app/src/main/java/com/jhanakdidwania/ioki/MainActivity.java
+++ app/src/main/java/com/jhanakdidwania/ioki/MainActivity.java
@@ -2,15 +2,13 @@ package com.jhanakdidwania.ioki;
 
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import java.io.IOException;
-import java.net.URL;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,60 +19,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mName = (EditText) findViewById(R.id.name);
-        mPassword = (EditText) findViewById(R.id.password);
+        mName = findViewById(R.id.name);
+        mPassword = findViewById(R.id.password);
 
-        /*Intent afterRegisteration = getIntent();
+        /*Intent afterRegistration = getIntent();
 
-        if(afterRegisteration.hasExtra("name") && afterRegisteration.hasExtra("password")){
-            String name= afterRegisteration.getStringExtra("name");
-            String password = afterRegisteration.getStringExtra("password");
+        if(afterRegistration.hasExtra("name") && afterRegistration.hasExtra("password")){
+            String name= afterRegistration.getStringExtra("name");
+            String password = afterRegistration.getStringExtra("password");
             mName.setText(name);
             mPassword.setText(password);
         }*/
     }
 
     public void LoginIntoAccount(View view) {
-
          String name = mName.getText().toString();
          String password = mPassword.getText().toString();
-
-         URL LoginUrl = NetworkUtils.buildUrl(name, password);
-         Log.d("ioki", LoginUrl.toString());
-         new IOkiLoginTask().execute(LoginUrl);
-
+         new IOkiLoginTask().execute(name,password);
     }
 
     public void registerNewUser(View view) {
         Intent myIntent = new Intent(MainActivity.this, RegisterUserName.class);
         startActivity(myIntent);
-        finish();
     }
 
-    public class IOkiLoginTask extends AsyncTask<URL, Void, String> {
+    // Inner class to invoke POST request for user/login
+    public static class IOkiLoginTask extends AsyncTask<String, Void, String> {
+
+        // Stores response from server
+        String response = "";
 
         @Override
-        protected String doInBackground(URL... params) {
-            URL searchUrl = params[0];
-            String LoginResults = null;
-            try {
-                LoginResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return LoginResults;
+        protected String doInBackground(String... params) {
+            // Making map of POST parameters
+            HashMap<String, String> dataParams = new HashMap<>();
+            dataParams.put("name", params[0]);
+            dataParams.put("pass",params[1]);
+
+            // Generating URL for POST request
+            String requestURL = NetworkUtils.IoKi_BASE_URL + "user/login/";
+
+            // Fetching response using utility function
+            response = NetworkUtils.performPostCall(requestURL,dataParams);
+            return response;
         }
 
         @Override
         protected void onPostExecute(String queryResults) {
-            if (queryResults != null && !queryResults.equals("")) {
-                Log.d("ioki", queryResults);
+            if (response!= null && !response.equals("")) {
+                Log.d("ioki", "Data received at server:");
+                Log.d("ioki", response);
             }else{
-                Log.d("ioki","No results fetched from login");
+                Log.d("ioki","No results fetched from user/login/");
             }
         }
     }
 }
-
 
 //cover flow for various credentials

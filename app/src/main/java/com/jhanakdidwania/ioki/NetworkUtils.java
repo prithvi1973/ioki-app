171f0a846d324e2304fb4e8ec44e15b3040bbf21
Prithvi Anand <prithvi.anand1972.pa@gmail.com>
2018-06-14 10:07:52 +0530

    Added POST functionality
    Moved data elements to user class. Added utility function for generating POST request.

--- app/src/main/java/com/jhanakdidwania/ioki/NetworkUtils.java
+++ app/src/main/java/com/jhanakdidwania/ioki/NetworkUtils.java
@@ -1,105 +1,95 @@
 package com.jhanakdidwania.ioki;
 
-
-import android.net.Uri;
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Scanner;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import javax.net.ssl.HttpsURLConnection;
 
 /**
- * These utilities will be used to communicate with the network.
+ * These utilities will be used to communicate with the server.
  */
 public class NetworkUtils {
 
-    final static String ip = "172.31.113.81";
-    final static String IOki_BASE_URL =
-           "http://" + ip + "/users";
-    final static String IOki_REGISTER_URL =
-            "http://" + ip + "/users/register";
+    /** Base url will look like https://www.ioki.com
+     *  When required, users/login/ or users/register/ can be appended to it
+     *  to generate usage specific URLs
+     */
+    final static String IoKi_BASE_URL = "http://pasf.netau.net/ioki/";
 
-    final static String LOGIN_PARAM_QUERY_NAME = "name";
-    final static String LOGIN_PARAM_QUERY_PASSWORD = "pass";
+    /** Helper function for performPostDataString
+     *  Reads a hash map of parameters
+     *  Prepares and returns string with parameters POST query
+     *  Example: <name:jhanak,pass:1234,type:1> becomes "name=jhanak&pass=1234&type=1"
+     */
+    private static String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
 
-    static String username;
-    static String name;
-    static String password;
-    static String confirmp;
-    static String mobile;
-    static String PIN;
-    static String email;
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setConfirmp(String confirmp) {
-        this.confirmp = confirmp;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public void setPIN(String PIN) {
-        this.PIN = PIN;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public static URL buildUrl(String LoginQueryName, String LoginQueryPassword) {
-        Uri builtUri = Uri.parse(IOki_BASE_URL).buildUpon()
-                .appendQueryParameter(LOGIN_PARAM_QUERY_NAME, LoginQueryName)
-                .appendQueryParameter(LOGIN_PARAM_QUERY_PASSWORD, LoginQueryPassword)
-                .build();
-
-        URL url = null;
-        try {
-            url = new URL(builtUri.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
         }
 
-        return url;
+        return result.toString();
     }
 
     /**
-     * This method returns the entire result from the HTTP response.
-     *
-     * @param url The URL to fetch the HTTP response from.
-     * @return The contents of the HTTP response.
-     * @throws IOException Related to network and stream reading
+     * Establishes an HTTP Connection
+     * Posts data to requestURL with postDataParams
+     * Fetches response stream
+     * Builds and returns response as string
      */
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("POST");
+    public static String  performPostCall(String requestURL, HashMap<String, String> postDataParams) {
+
+        URL url;
+        StringBuilder response = new StringBuilder();
         try {
-            InputStream in = urlConnection.getInputStream();
+            url = new URL(requestURL);
 
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
 
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode=conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br=new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line=br.readLine()) != null) {
+                    response.append(line);
+                }
             }
-        } finally {
-            urlConnection.disconnect();
+            else {
+                response = new StringBuilder();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return response.toString();
     }
+
 }

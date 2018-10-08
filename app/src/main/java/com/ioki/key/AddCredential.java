33932dca11500393eb7c883fa7ef935cf837866e
jhanak didwania <jhanakdidwania99@gmail.com>
2018-10-09 03:10:16 +0530

    1. Completed activity addLocks and addCredentials 2. Added spinner list adapter in addCredentials
    TODO: Sent the request for new lock or credential to the server for registration

--- app/src/main/java/com/ioki/key/AddCredential.java
+++ app/src/main/java/com/ioki/key/AddCredential.java
@@ -1,14 +1,23 @@
 package com.ioki.key;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class AddCredential extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddCredential extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     EditText link, login, password;
+    Spinner spinner;
+    String typeSelected = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,9 +26,48 @@ public class AddCredential extends AppCompatActivity {
         link = findViewById(R.id.link);
         login = findViewById(R.id.loginAddCredential);
         password = findViewById(R.id.passwordAddCredentials);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Username");
+        categories.add("Email");
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        typeSelected = parent.getItemAtPosition(position).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {}
+
     public void addANewCredential(View view) {
+        if(link.getText().toString().equals("") || login.getText().toString().equals("")
+                || password.getText().toString().equals("")){
+            Toast.makeText(this, "Fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+        else if(typeSelected.equals("")){
+            Toast.makeText(this, "Select a type", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            // TODO: Register new credential on the server
+            Intent intent = new Intent(this, Dashboard.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
--- app/src/main/java/com/ioki/key/AddLock.java
+++ app/src/main/java/com/ioki/key/AddLock.java
@@ -1,11 +1,13 @@
 package com.ioki.key;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddLock extends AppCompatActivity{
 
@@ -21,11 +23,17 @@ public class AddLock extends AppCompatActivity{
     }
 
     public void addANewLock(View view) {
-
         String id = LockId.getText().toString();
         String name = LockName.getText().toString();
 
-        // TODO: add the newly created lock to recycler view
-        // TODO: register new lock on server
+        if(id.equals("") || name.equals("")){
+            Toast.makeText(this, "Fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            // TODO: register new lock on server
+            Intent intent = new Intent(this, Dashboard.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
